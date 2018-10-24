5fa32de2a4c16a6056fa73dc1c4c9b4bfdcc3239
GeovaneNarciso <geovane.nivel4@gmail.com>
2018-10-24 09:19:35 -0800

    Repositório - Criação método status,   Arquivo - Criação de Gets

--- Atv_F/git_modelos.py
+++ Atv_F/git_modelos.py
@@ -4,11 +4,28 @@ class Repositorio:
         self.arquivos = []
         self.commits = []
 
+    def status(self):
+        print("Arquivos Untracked: \n")
+        for arquivo in self.arquivos:
+            if not arquivo.get_tracked():
+                print(arquivo.get_nome, "\n")
+        print("\nArquivos Tracked: \n")
+        for arquivo in self.arquivos:
+            if  arquivo.get_tracked():
+                print(arquivo.get_nome, "\n")
+
 class Arquivo:
     def __init__(self, nome, conteudo):
         self.nome = nome
         self.conteudo = conteudo
         self.mudanca = []
+        self.tracked = False
+
+    def get_tracked(self):
+        return self.tracked
+
+    def get_nome(self):
+        return self.nome
 
 class Mudanca:
     pass
