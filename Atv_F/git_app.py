48f9a3d465127f7dc6b7f8b20936bb8632ba91a5
GeovaneNarciso <geovane.nivel4@gmail.com>
2018-10-26 10:48:32 -0800

    App - Criação da opção Add.

--- Atv_F/git_app.py
+++ Atv_F/git_app.py
@@ -11,20 +11,29 @@ def main():
                   "1 - Criar repositório\n"
                   "2 - Criar arquivo\n"
                   "3 - Listar arquivos\n"
+                  "5 - Adicionar arquivo\n"
                   "8 - Status\n"
                   "0 - Sair\n")
         opcao = int(input("Informe a opção: "))
         if opcao == 1:
             nome_repo = input("Informe o nome do repositório: \n")
             repo_01 = Repositorio(nome_repo)
+
         elif opcao == 2:
             nome_arq = input("Informe o nome do arquivo: ")
             conteudo_arq = input("Informe o conteudo do arquivo: ")
             print(repo_01.criar_arquivo(nome_arq, conteudo_arq))
+
         elif opcao == 3:
             repo_01.listar_arquivos()
+
+        elif opcao == 5:
+            nome_arq = input("Informe o nome do arquivo: ")
+            print(repo_01.add(nome_arq))
+
         elif opcao == 8:
             repo_01.status()
+
         elif opcao == 0:
             print("Encerrado.")
 
