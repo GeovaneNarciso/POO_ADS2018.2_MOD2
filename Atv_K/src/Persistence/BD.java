aee5dcb78326001e05f74948b05127307724a33a
GeovaneNarciso <geovane.nivel4@gmail.com>
2018-11-21 12:17:33 -0800

    Criado comentário na classe BD.

--- Atv_K/src/Persistence/BD.java
+++ Atv_K/src/Persistence/BD.java
@@ -12,14 +12,14 @@ public class BD {
 
     public Quadro get(int id){
         for (Quadro q : this.quadros){
-            if (q.getId().equals(id)){
+            if (q.getId().equals(id)){ //Atributo id ainda não criado.
                 return q;
             }
         }
         return null;
     }
 
-    public void update(){
+    public void update(Quadro q){
         for (Quadro q2 : this.quadros){
             if (q2.getId().equals(q.getId))
                 q2 = q;
