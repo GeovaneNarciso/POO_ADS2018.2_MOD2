d86b38e31dbd7b63dac390cc5ae77413bde03881
GeovaneNarciso <geovane.nivel4@gmail.com>
2018-12-05 20:59:40 -0800

    Criado o layout da Activity 1.

--- Atv_M/app/src/main/java/g/nu3ents/MainActivity.java
+++ Atv_M/app/src/main/java/g/nu3ents/MainActivity.java
@@ -2,6 +2,8 @@ package g.nu3ents;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = findViewById(R.id.estilo_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.estilo_spinner, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
