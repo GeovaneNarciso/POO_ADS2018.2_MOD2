7209fe3d20fa5052c392acb26368ab3f341f80a5
GeovaneNarciso <geovane.nivel4@gmail.com>
2019-02-04 11:05:14 -0300

    Atualização

--- Atv_X_Final/stop/app/src/main/java/stopthegame/com/stopthegame/SalaInicioActivity.java
+++ Atv_X_Final/stop/app/src/main/java/stopthegame/com/stopthegame/SalaInicioActivity.java
@@ -8,12 +8,15 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SalaInicioActivity extends AppCompatActivity {
     private DatabaseReference stopBD;
     private TextView cod_sala;
+    private TextView rodadas;
 
     private String valor_cod_sala;
+    private String qtd_max_rodadas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +24,18 @@ public class SalaInicioActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sala_inicio);
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            valor_cod_sala = user.getUid().substring(user.getUid().length() - 5);
-        }
+
         cod_sala = findViewById(R.id.textViewCodSalaValor);
+        rodadas = findViewById(R.id.textViewRodadaValor);
+
+        if (user != null)
+            valor_cod_sala = user.getUid().substring(user.getUid().length() - 5);
         cod_sala.setText(valor_cod_sala);
+
+        stopBD = FirebaseDatabase.getInstance().getReference().child("salas").child(valor_cod_sala)
+        .child("qtdMaxRodadas");
+        qtd_max_rodadas = stopBD.getKey();
+        rodadas.setText(qtd_max_rodadas);
+
     }
 }
